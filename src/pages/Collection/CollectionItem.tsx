8f76da40766940e2ffefd672dcda402274bf4846
Joseph Kim <josephikim2@gmail.com>
2022-06-08 14:20:16 -0700

    add drink name and rating to top of CollectionItem

--- src/pages/Collection/CollectionItem.tsx
+++ src/pages/Collection/CollectionItem.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Container, Tabs, Tab, Row, Col } from 'react-bootstrap';
+import { Container, Tabs, Tab, Row, Col, Image } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import StarRatingComponent from 'react-star-rating-component';
 
 import { IUserCollectionItemDoc } from '../../db/UserCollectionItem';
 import { useAppSelector, useAppDispatch } from '../../hooks';
 import { RatingPayload, setRating } from '../../store/userSlice';
+import ContentWrapper from '../../layout/ContentWrapper';
 import DrinkInfo from '../../components/DrinkInfo';
 import DrinkNotes from '../../components/DrinkNotes';
 import DeleteCollectionItemButton from '../../components/DeleteCollectionItemButton';
@@ -45,20 +46,36 @@ const CollectionItem: React.FC = () => {
             <Tab eventKey="info" title="Info">
               <Row>
                 <Col>
+                  <div className="tab-content-cont__info">
+                    <Row>
+                      <Col md={5}>
+                        <ContentWrapper>
+                          <Image width={250} height={250} src={matchingDrink.strDrinkThumb} fluid />
+                        </ContentWrapper>
+                      </Col>
+                      <Col md={7}>
+                        <Row>
+                          <ContentWrapper>
+                            <h4>{matchingDrink.strDrink}</h4>
+                            <StarRatingComponent
+                              name={collectionItem.idDrink}
+                              starCount={5}
+                              value={collectionItem.rating ? (collectionItem.rating as number) : 0}
+                              onStarClick={(nextValue, prevValue, name) => onStarClick(nextValue, prevValue, name)}
+                            />
+                            <p>My Rating</p>
+                          </ContentWrapper>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
                   <DrinkInfo />
                 </Col>
               </Row>
-            </Tab>
-            <Tab eventKey="rating" title="Rating">
-              <div className="tab-content-cont__rating">
-                <p>Enter a rating from 1 to 5</p>
-                <StarRatingComponent
-                  name={collectionItem.idDrink}
-                  starCount={5}
-                  value={collectionItem.rating ? (collectionItem.rating as number) : 0}
-                  onStarClick={(nextValue, prevValue, name) => onStarClick(nextValue, prevValue, name)}
-                />
-              </div>
             </Tab>
             <Tab eventKey="notes" title="Notes">
               <DrinkNotes notes={collectionItem.notes as string} idDrink={collectionItem.idDrink} />

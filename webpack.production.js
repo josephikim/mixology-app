eeff5931d01d167be1e36c0bd1af612880a56939
Joseph Kim <josephikim2@gmail.com>
2022-06-24 20:18:21 -0700

    add production build scripts

--- webpack.production.js
+++ webpack.production.js
@@ -1,21 +1,51 @@
+import path from 'path';
 import { merge } from 'webpack-merge';
 import Dotenv from 'dotenv-webpack';
-import common from './webpack.common';
+import nodeExternals from 'webpack-node-externals';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+import common from './webpack.common.js';
+
 const client = merge(common, {
+  name: 'client',
+  entry: './src/index.tsx',
+  target: 'web',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
   plugins: [
-    new Dotenv({
-      path: '.env.production'
+    new HtmlWebpackPlugin({
+      template: path.resolve('src', 'index.html'),
+      filename: 'index.html'
     }),
-    new HtmlWebpackPlugin()
+    new Dotenv({
+      path: '.env.development'
+    })
   ]
 });
 
 const server = merge(common, {
-  // prod server configs
+  name: 'server',
+  target: 'node',
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  entry: './src/server.ts',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'server.cjs'
+  },
+  externals: [nodeExternals()],
+  plugins: [
+    new Dotenv({
+      path: '.env.production'
+    })
+  ]
 });
 
-const config = { ...client, ...server };
+const config = [client, server];
 
 export default config;

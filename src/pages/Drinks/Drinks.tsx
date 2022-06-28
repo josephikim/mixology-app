24a7ea6b9ee8252177d2e9e7d7b71e2ee124ae8c
Joseph Kim <josephikim2@gmail.com>
2022-06-28 16:30:58 -0700

    add getDrinks hooks in Drinks page, update render logic

--- src/pages/Drinks/Drinks.tsx
+++ src/pages/Drinks/Drinks.tsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { cloneDeep } from 'lodash';
 
-import { useAppSelector } from 'hooks';
+import { useAppSelector, useAppDispatch } from 'hooks';
+import { getDrinks } from 'store/baseSlice';
 import DrinksItemHeader from './DrinksItemHeader';
 import DrinksItem from './DrinksItem';
 
 import './Drinks.css';
 
 const Drinks: React.FC = () => {
+  const dispatch = useAppDispatch();
   const drinks = useAppSelector((state) => state.base.drinks);
   const drinksClone = cloneDeep(drinks);
-  const drinksSorted = drinksClone.sort((a, b) => (a.strDrink as string).localeCompare(b.strDrink as string));
+  const isDrinksLoaded = drinksClone?.length && drinksClone.length > 0;
 
-  return (
-    <div className="Drinks">
-      <Container>
+  useEffect(() => {
+    if (!isDrinksLoaded) {
+      dispatch(getDrinks());
+    }
+  }, []);
+
+  const renderContent = () => {
+    if (isDrinksLoaded) {
+      const drinksSorted = drinksClone.sort((a, b) => (a.strDrink as string).localeCompare(b.strDrink as string));
+      return (
         <div className="drinks-wrapper">
           <DrinksItemHeader />
           {drinksSorted.map((drink) => {
             return <DrinksItem key={drink.idDrink} drink={drink} />;
           })}
         </div>
-      </Container>
+      );
+    } else {
+      return (
+        <div className="drinks-wrapper">
+          <h4>Loading drinks...</h4>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="Drinks">
+      <Container>{renderContent()}</Container>
     </div>
   );
 };
